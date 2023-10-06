488e62f71e3ac5298901bfb887d6fb95181ad669
hoduulmu <timewarp96@naver.com>
2023-10-07 02:19:05 +0900

    feat: add DatabaseCleaner

--- src/test/java/com/kjh/wms/common/ApiTest.java
+++ src/test/java/com/kjh/wms/common/ApiTest.java
@@ -2,6 +2,7 @@ package com.kjh.wms.common;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -11,11 +12,16 @@ public class ApiTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
     @BeforeEach
     void setUp() {
         if (RestAssured.UNDEFINED_PORT == RestAssured.port) {
             RestAssured.port = port;
+            databaseCleaner.afterPropertiesSet();
         }
+        databaseCleaner.execute();
     }
 
 }
