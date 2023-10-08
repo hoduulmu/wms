d5cae8e5f5f88d73d430f052e4d6316d3329904d
hoduulmu <timewarp96@naver.com>
2023-10-08 17:54:04 +0900

    feat: to Scenario Test

--- src/test/java/com/kjh/wms/common/Scenario.java
+++ src/test/java/com/kjh/wms/common/Scenario.java
@@ -1,10 +1,15 @@
 package com.kjh.wms.common;
 
+import com.kjh.wms.inbound.feature.api.RegisterInboundApi;
 import com.kjh.wms.product.feature.api.RegisterProductApi;
 
 public class Scenario {
 
     public static RegisterProductApi registerProduct() {
         return new RegisterProductApi();
+    }
+
+    public RegisterInboundApi registerInbound() {
+        return new RegisterInboundApi();
     }
 }
--- src/test/java/com/kjh/wms/inbound/feature/RegisterInboundTest.java
+++ src/test/java/com/kjh/wms/inbound/feature/RegisterInboundTest.java
@@ -1,27 +1,15 @@
 package com.kjh.wms.inbound.feature;
 
 import com.kjh.wms.common.ApiTest;
+import com.kjh.wms.common.Scenario;
 import com.kjh.wms.inbound.domain.InboundRepository;
-import com.kjh.wms.product.domain.ProductRepository;
-import com.kjh.wms.product.fixturer.ProductFixture;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RegisterInboundTest extends ApiTest {
-
-    @MockBean
-    private ProductRepository productRepository;
 
     @Autowired
     private InboundRepository inboundRepository;
@@ -29,39 +17,9 @@ class RegisterInboundTest extends ApiTest {
     @Test
     @DisplayName("입고를 등록한다")
     void registerInbound() {
-        Mockito.when(productRepository.getBy(Mockito.anyLong()))
-                .thenReturn(ProductFixture.aProduct().build());
-
-        final Long productNo = 1L;
-        final Long quantity = 1L;
-        final Long unitPrice = 1500L;
-        final RegisterInbound.Request.Item inboundItem = new RegisterInbound.Request.Item(
-                productNo,
-                quantity,
-                unitPrice,
-                "description"
-        );
-
-        final LocalDateTime orderRequestedAt = LocalDateTime.now();
-        final LocalDateTime estimateArrivalAt = LocalDateTime.now().plusDays(1);
-        final List<RegisterInbound.Request.Item> inboundItems = List.of(inboundItem);
-        final RegisterInbound.Request request = new RegisterInbound.Request(
-                "title",
-                "description",
-                orderRequestedAt,
-                estimateArrivalAt,
-                inboundItems
-        );
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when()
-                .post("/inbounds")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+        Scenario.registerProduct().request()
+                .registerInbound().request();
 
         assertThat(inboundRepository.findAll()).hasSize(1);
     }
-
 }
