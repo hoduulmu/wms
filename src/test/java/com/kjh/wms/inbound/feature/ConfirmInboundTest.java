89d854196a8c25e22fc9c4637ec9b7df881d9832
hoduulmu <timewarp96@naver.com>
2023-10-10 10:30:20 +0900

    feat: ConfirmInbound to API

--- src/test/java/com/kjh/wms/inbound/feature/ConfirmInboundTest.java
+++ src/test/java/com/kjh/wms/inbound/feature/ConfirmInboundTest.java
@@ -1,34 +1,37 @@
 package com.kjh.wms.inbound.feature;
 
-import com.kjh.wms.inbound.domain.*;
-import org.junit.jupiter.api.BeforeEach;
+import com.kjh.wms.common.ApiTest;
+import com.kjh.wms.common.Scenario;
+import com.kjh.wms.inbound.domain.Inbound;
+import com.kjh.wms.inbound.domain.InboundRepository;
+import com.kjh.wms.inbound.domain.InboundStatus;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ConfirmInboundTest {
+class ConfirmInboundTest extends ApiTest {
 
-    private ConfirmInbound confirmInbound;
+    @Autowired
     private InboundRepository inboundRepository;
-
-    @BeforeEach
-    void setUp() {
-        inboundRepository = Mockito.mock(InboundRepository.class);
-        confirmInbound = new ConfirmInbound(inboundRepository);
-    }
 
     @Test
     @DisplayName("입고를 승인한다")
     void confirmInbound() {
+
+        Scenario.registerProduct().request()
+                .registerInbound().request();
         final Long inboundNo = 1L;
-        final Inbound inbound = InboundFixture.anInbound().build();
-        Mockito.when(inboundRepository.getBy(inboundNo))
-                .thenReturn(inbound);
 
-        confirmInbound.request(inboundNo);
+        RestAssured.given().log().all()
+                .when().post("/inbounds/{inboundNo}/confirm", inboundNo)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
 
+        final Inbound inbound = inboundRepository.getBy(inboundNo);
         assertThat(inbound.getStatus()).isEqualTo(InboundStatus.CONFIRMED);
     }
 }
