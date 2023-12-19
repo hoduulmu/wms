e0b166390e1a505baa7d305836952d5df6ac68be
hoduulmu <timewarp96@naver.com>
2023-12-20 08:56:01 +0900

    feat: create test case register location

--- src/test/java/com/kjh/wms/location/feature/RegisterLocationTest.java
+++ src/test/java/com/kjh/wms/location/feature/RegisterLocationTest.java
@@ -1,4 +1,15 @@
 package com.kjh.wms.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterLocationTest {
+
+    @Test
+    @DisplayName("로케이션을 등록한다")
+    void registerLocation() {
+        // given
+        // when
+        // then
+    }
 }
