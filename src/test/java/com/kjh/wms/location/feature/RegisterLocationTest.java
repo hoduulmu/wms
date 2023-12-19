bce2ad7ef77c5d4a231a0cbfd8af981e08ae49f7
hoduulmu <timewarp96@naver.com>
2023-12-20 08:58:04 +0900

    feat: create inner class RegisterLocation

--- src/test/java/com/kjh/wms/location/feature/RegisterLocationTest.java
+++ src/test/java/com/kjh/wms/location/feature/RegisterLocationTest.java
@@ -1,15 +1,24 @@
 package com.kjh.wms.location.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterLocationTest {
 
+
+    private RegisterLocation registerLocation;
+
+    @BeforeEach
+    void setUp() {
+        registerLocation = new RegisterLocation();
+    }
+
     @Test
     @DisplayName("로케이션을 등록한다")
     void registerLocation() {
-        // given
-        // when
-        // then
+    }
+
+    private class RegisterLocation {
     }
 }
