031915de0bf12c68575b1a151f827bf38a690d7f
hoduulmu <timewarp96@naver.com>
2023-12-21 09:26:55 +0900

    feat: add signature of Request

--- src/test/java/com/kjh/wms/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/kjh/wms/location/feature/AssignLocationLPNTest.java
@@ -3,6 +3,7 @@ package com.kjh.wms.location.feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class AssignLocationLPNTest {
 
@@ -16,7 +17,9 @@ class AssignLocationLPNTest {
     @Test
     @DisplayName("로케이션에 LPN 할당한다")
     void assignLocationLPN() {
-        AssignLocationLPN.Request request = new AssignLocationLPN.Request();
+        final String locationBarcode = "A-1-1";
+        final String lpnBarcode = "LPN-1";
+        final AssignLocationLPN.Request request = new AssignLocationLPN.Request(locationBarcode, lpnBarcode);
         assignLocationLPN.request(request);
     }
 
@@ -25,7 +28,12 @@ class AssignLocationLPNTest {
 
         }
 
-        public record Request() {
+        public record Request(String locationBarcode,
+                              String lpnBarcode) {
+            public Request {
+                Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
+                Assert.hasText(lpnBarcode, "LPN 바코드는 필수입니다.");
+            }
         }
     }
 }
