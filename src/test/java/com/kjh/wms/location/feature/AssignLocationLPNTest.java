8e32cf856f26a2c92de296015f2a0ea5fbce8e66
hoduulmu <timewarp96@naver.com>
2023-12-21 09:18:42 +0900

    feat: create inner class AssignLocationLPN

--- src/test/java/com/kjh/wms/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/kjh/wms/location/feature/AssignLocationLPNTest.java
@@ -1,15 +1,23 @@
 package com.kjh.wms.location.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AssignLocationLPNTest {
 
+    private AssignLocationLPN assignLocationLPN;
+
+    @BeforeEach
+    void setUp() {
+        assignLocationLPN = new AssignLocationLPN();
+    }
+
     @Test
     @DisplayName("로케이션에 LPN 할당한다")
     void assignLocationLPN() {
-        // given
-        // when
-        // then
+    }
+
+    private class AssignLocationLPN {
     }
 }
