f5a8d6f0b307dbd1632ac063e031cef5f4470e44
hoduulmu <timewarp96@naver.com>
2024-01-02 12:56:31 +0900

    feat: create test: Location#assignLPN

---
+++ src/test/java/com/kjh/wms/location/domain/LocationTest.java
@@ -0,0 +1,14 @@
+package com.kjh.wms.location.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LocationTest {
+
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다")
+    void assignLPN() {
+
+    }
+
+}
