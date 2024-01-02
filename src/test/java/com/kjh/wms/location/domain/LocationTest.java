7c2badd194da638f01dc140c16ee1687b6f1a911
hoduulmu <timewarp96@naver.com>
2024-01-02 13:18:05 +0900

    test: add assertion

--- src/test/java/com/kjh/wms/location/domain/LocationTest.java
+++ src/test/java/com/kjh/wms/location/domain/LocationTest.java
@@ -1,14 +1,21 @@
 package com.kjh.wms.location.domain;
 
+import com.kjh.wms.inbound.domain.LPN;
+import com.kjh.wms.inbound.domain.LPNFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LocationTest {
 
     @Test
     @DisplayName("로케이션에 LPN을 할당한다")
     void assignLPN() {
-
+        Location location = LocationFixture.aLocation().build();
+        LPN lpn = LPNFixture.anLPN().build();
+        location.assignLPN(lpn);
+        assertThat(location.getLocationLPNs()).hasSize(1);
     }
 
 }
